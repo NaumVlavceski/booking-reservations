94c46b2b043696ea189c91ea8f89d5abff33c722
NaumVlavceski <nvlavceski542@gmail.com>
2026-09-20 11:24:08 +0200

    first commit

---
+++ backend/src/test/java/com/naumvlavceski/bookingbackend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naumvlavceski.bookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
