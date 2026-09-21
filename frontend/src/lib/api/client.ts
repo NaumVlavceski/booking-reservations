598adc47130d38dc58c78b969fc152e24a43129c
NaumVlavceski <nvlavceski542@gmail.com>
2026-09-21 20:10:38 +0200

    add port for production database

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,26 +1,28 @@
 import axios from "axios";
-import { tokenStorage } from "../auth/tokenStorage";
+import {tokenStorage} from "../auth/tokenStorage";
 
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8080",
 });
 
-apiClient.interceptors.request.use((config) =>{
+apiClient.interceptors.request.use((config) => {
     const token = tokenStorage.get();
-    if (token){
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 });
 
 apiClient.interceptors.response.use(
-    (response)=>response,
-    (error) =>{
-    if(error.response?.status === 401){
-        tokenStorage.clear();
-        window.location.href = "/login";
-    }
-    return Promise.reject(error);
-}
+    (response) => response,
+    (error) => {
+        const hadToken = !!tokenStorage.get();
 
-)
+        if (error.response?.status === 401 && hadToken) {
+            tokenStorage.clear();
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
+    }
+);
